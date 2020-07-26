133a991454547dfe71fe5e48d1425253d62a2674
tarunj096 <58319215+tarunj096@users.noreply.github.com>
2020-07-26 23:43:20 +0530

    Added Files

---
+++ Java-Exercises/Two_Dimension_Array/twoDArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class twoDArray {
+	public static void main(String[] args) {
+		int arr[][]= {{1,9,2,2,3,4},{5,6,7,8,3,4}};
+				for(int i=0;i<arr.length;i++) {
+					for(int j=0;j<arr[i].length;j++) {
+						System.out.print(arr[i][j]+" " );
+					}
+				System.out.println();
+					}
+			
+		}
+		
+	}
